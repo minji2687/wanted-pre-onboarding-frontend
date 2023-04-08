b1d5859e9b8c32d0c641974d963b3aa1a6bca1e4
Jo Minji <mingi2687@gamil.com>
2023-04-08 20:30:56 +0900

    feat: 라우트 설정

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Todo from './components/Todo';
+import AuthRoute from './components/AuthRoute'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<App/>}/>
+      <Route path={"/signup"} element={<SignUp/>}/>
+      <Route path={"/signin"} element={<SignIn/>}/>
+      <Route exact path='/todo' element={<AuthRoute/>}>
+        <Route exact path='/todo' element={<Todo/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
